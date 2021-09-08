15bd011ffcf8af5d47e73db9e334bb8ec7a23c2f
uri-yanover <uri.yanover+github@gmail.com>
2021-09-08 12:07:37 +0300

    Use stock urllib and not urllib3

--- games.py
+++ games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import urllib3
-import re
+import urllib.request
+import urllib.parse
 import json
 import sys
 from typing import List
 from calendar import monthrange
+
 
 def get_game_data(year: int, month: int) -> List[dict]:
     if year < 1900:
@@ -13,7 +14,6 @@ def get_game_data(year: int, month: int) -> List[dict]:
         raise ValueError('Month must be an integer in the range 1..12')
 
     # Creating a PoolManager instance for sending requests.
-    http = urllib3.PoolManager()
     page = 0
     result = []
     last_day = monthrange(year, month)[1]
@@ -23,21 +23,31 @@ def get_game_data(year: int, month: int) -> List[dict]:
 
     # Sending a GET request and getting back response as HTTPResponse object.
     while True:
-        resp = http.request("GET", f"https://www.balldontlie.io/api/v1/games?start_date={start_date}&end_date={end_date}&page={page}&per_page=100")
+        url_string = urllib.parse.urlunparse(
+            urllib.parse.ParseResult(scheme='https', netloc='www.balldontlie.io', path='/api/v1/games',
+                                     params='', query=urllib.parse.urlencode(
+                                         (('start_date', start_date),
+                                          ('end_date', end_date),
+                                          ('page', page),
+                                          ('per_page', 100))
+                                     ), fragment=''))
 
-        if resp.status != 200:
-            raise RuntimeError(f"Received invalid status {resp.status}")
+        with urllib.request.urlopen(url_string) as resp:
+            if resp.status != 200:
+                raise RuntimeError(f"Received invalid status {resp.status}")
 
-        # Print the returned data.
-        parsed = json.loads(resp.data)
-        result.extend(parsed.get('data', []))
-        page = parsed.get("meta", {}).get("next_page")
+            # Print the returned data.
+            parsed = json.loads(resp.read())
+            result.extend(parsed.get('data', []))
+            page = parsed.get("meta", {}).get("next_page")
 
         if page is None:
             break
     return result
 
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         raise RuntimeError(f'{sys.argv[0]} <year> <month>')
-    print(json.dumps(get_game_data(int(sys.argv[1]), int(sys.argv[2])), indent=4))
+    print(json.dumps(get_game_data(
+        int(sys.argv[1]), int(sys.argv[2])), indent=4))
